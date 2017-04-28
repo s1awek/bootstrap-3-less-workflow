e18135e2da732664a94f626d1f0ed22462edd5ec
s1awek <elvizky@gmail.com>
2017-04-28 15:03:00 +0200

    Added console and nice svg animation in the background

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+/*jslint browser:true */
+/*global $:false, jQuery:false */
 
+$(document).ready(function ($) {
+    'use strict';
+    //Toggle .collapsed class for animation of burger icon
+    $('.navbar-toggle').on('click', function () {
+        $(this).toggleClass('collapsed');
+    });
+
+    //Control SVG animoation at ABOUT_ME section
+    setTimeout(function () {
+        $('.img-bg').fadeIn(3000);
+        $('.img-stroke').fadeOut(3000);
+    }, 3000);
+
+});
