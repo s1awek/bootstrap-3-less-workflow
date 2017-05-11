4a4040116b80844edcf4c2d0a3e02a155c62f4e5
Slawek <elvizky@gmail.com>
2017-05-11 13:47:28 +0200

    This commit is just maintance commit, after my Windows 7 was reinstalled

--- js/main.js
+++ js/main.js
@@ -1,55 +1,54 @@
-/*jslint browser:true */
-/*global $:false, jQuery:false, console:false */
+/* jslint browser:true */
+/* global $:false, console:false */
 
-$(document).ready(function ($) {
-    'use strict';
-    //Toggle .collapsed class for animation of burger icon
-    $('.navbar-toggle').on('click', function () {
-        $(this).toggleClass('collapsed');
-    });
+$(document).ready(function($) {
+  'use strict';
+  // Toggle .collapsed class for animation of burger icon
+  $('.navbar-toggle').on('click', function() {
+    $(this).toggleClass('collapsed');
+  });
 
-    //Control SVG animoation at ABOUT_ME section
-    setTimeout(function () {
-        $('.img-bg').fadeIn(3000);
-        $('.img-stroke').fadeOut(3000);
-    }, 3000);
+  // Control SVG animoation at ABOUT_ME section
+  setTimeout(function() {
+    $('.img-bg').fadeIn(3000);
+    $('.img-stroke').fadeOut(3000);
+  }, 3000);
 
+  $('.img-stroke').attr('src', 'img/abc_a.svg?' + Math.random()); // append random query string to animated SVG to prevent browser caching and skipping animation.
 
-    $('.img-stroke').attr('src', 'img/abc_a.svg?' + Math.random()); //append random query string to animated SVG to prevent browser caching.
-    //Caching was cosing animation not playing.
+  // idea of determine scroll direction taken from:
+  // http://stackoverflow.com/questions/4326845/how-can-i-determine-the-direction-of-a-jquery-scroll-event
+  var lastScrollTop = 0;
+  var scrollSpeed = 2.0;
+  $(window).scroll(function(event) {
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop) { // if scroll down
+      $('.img-bg').css('top', '-' + Math.floor($(window).scrollTop() * scrollSpeed) + 'px');
+      $('.img-stroke').css('top', '-' + Math.floor($(window).scrollTop() * scrollSpeed) + 'px');
+    } else { // if scroll up
+      $('.img-bg').css('top', '-' + Math.floor($(window).scrollTop() * scrollSpeed) + 'px');
+      $('.img-stroke').css('top', '-' + Math.floor($(window).scrollTop() * scrollSpeed) + 'px');
+    }
+    lastScrollTop = st;
+  });
 
-    //idea of determine scroll direction taken from:
-    //http://stackoverflow.com/questions/4326845/how-can-i-determine-the-direction-of-a-jquery-scroll-event
-    var lastScrollTop = 0,
-        scrollSpeed = 2.0;
-    $(window).scroll(function (event) {
-        var st = $(this).scrollTop();
-        if (st > lastScrollTop) { //if scroll down
-            $('.img-bg').css('top', '-' + Math.floor($(window).scrollTop() * scrollSpeed) + 'px');
-            $('.img-stroke').css('top', '-' + Math.floor($(window).scrollTop() * scrollSpeed) + 'px');
-        } else { //if scroll up
-            $('.img-bg').css('top', '-' + Math.floor($(window).scrollTop() * scrollSpeed) + 'px');
-            $('.img-stroke').css('top', '-' + Math.floor($(window).scrollTop() * scrollSpeed) + 'px');
-        }
-        lastScrollTop = st;
+  $('.ui-x').click(function() {
+    $('.console').css('display', 'none');
+  });
+  $('.ui-up').click(function() {
+    $('.console').css({
+      'height': '20px',
+      'resize': 'none',
+      'overflow': 'hidden',
+      'padding': '13px'
     });
-    $('.ui-x').click(function () {
-        $('.console').css('display', 'none');
+  });
+  $('.ui-down').click(function() {
+    $('.console').css({
+      'height': '350px',
+      'resize': 'both',
+      'overflow': 'auto',
+      'padding': '40px 5px 5px'
     });
-    $('.ui-down').click(function () {
-        $('.console').css({
-            'height': '20px',
-            'resize': 'none',
-            'overflow': 'hidden',
-            'padding': '13px'
-        });
-    });
-    $('.ui-up').click(function () {
-        $('.console').css({
-            'height': '350px',
-            'resize': 'both',
-            'overflow': 'auto',
-            'padding': '40px 5px 5px'
-        });
-    });
+  });
 });
